5c2524f73930c8bd878aa40f9f1409a06cbefd32
carry0987 <carry0987@users.noreply.github.com>
2024-06-25 21:52:47 +0800

    Upload

---
+++ docusaurus.config.ts
@@ -0,0 +1,107 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+    title: 'Serapha',
+    tagline: 'A lightweight, modular PHP framework utilizing modern practices with template engine and Sanite for easy database CRUD operations.',
+    favicon: 'static/img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://seraphalab.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/Wiki/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'SeraphaLab', // Usually your GitHub org/user name.
+    projectName: 'Wiki', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/SeraphaLab/Wiki/tree/main/',
+                },
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'static/img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Serapha',
+            logo: {
+                alt: 'Serapha Logo',
+                src: 'static/img/logo.svg',
+                style: {
+                    borderRadius: '50%',
+                }
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Document',
+                },
+                {
+                    href: 'https://github.com/SeraphaLab/Serapha',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Document',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/SeraphaLab/Serapha',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} SeraphaLab. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;

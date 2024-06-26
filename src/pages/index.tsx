86c7000ccb1d71c55cbc0f8359cfc146092f73bd
carry0987 <carry0987@users.noreply.github.com>
2024-06-26 11:13:09 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title=''
-      description={`${siteConfig.tagline}`}>
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
